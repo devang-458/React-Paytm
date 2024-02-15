6440bc08fb207b0b181271c1a0c67b97dee6f9f0
Devang kushwah <deekayedm@gmail.com>
2024-02-15 11:27:20 +0530

    authmiddleware done

--- Backend/routes/middleware.js
+++ Backend/routes/middleware.js
@@ -1,3 +1,4 @@
+const { JWT_SECRET } = require("../conifg")
 
 const authMiddleware = (req,res,next)=>{
     const authHeader = req.headers.authorization
@@ -9,6 +10,15 @@ const authMiddleware = (req,res,next)=>{
     const token = authHeader.split(" ")[1]
 
     try{
-        const decoded = jwt
+        const decoded = jwt.verify(token,JWT_SECRET);
+
+        req.userId = decoded.userId;
+        next()
+    } catch(err){
+        return res.status(403).json({})
     }
+}
+
+module.exports = {
+    authMiddleware
 }
